43e1d8286df2865773449f791073ed3a4860eb2e
steveperry-53 <seperry53@gmail.com>
2016-12-21 10:11:05 -0800

    Add function variables to functions example.

--- 03-functions/functions.go
+++ 03-functions/functions.go
@@ -3,12 +3,39 @@ package main
 import (
   "fmt"
   "math"
+  "strconv"
 )
+
+type Thing struct {
+  s string
+  Run func(string, int) string
+}
+
 
 func main() {
   fmt.Println(math.Sqrt(37))
   fmt.Println( AddAndMultiply(6, 7) )
   fmt.Println( swap(55, "Steve") )
+
+  av := func(u, v float64) float64 {
+          return (u + v)/2
+        }
+
+  fmt.Println( ComputeWith34(Area) )
+  fmt.Println( ComputeWith34(Perimeter) )
+  fmt.Println( ComputeWith34(math.Pow) )
+  fmt.Println( ComputeWith34(hypot) )
+  fmt.Println( ComputeWith34(av) )
+
+  t1 := Thing{
+    s: "Steve",
+    Run: func(name string, age int) string {
+      return name + " Perry is " + strconv.Itoa(age) + "."
+    },
+  }
+
+  fmt.Println( t1.s, t1.Run("Edward", 64) )
+
 }
 
 
@@ -20,3 +47,22 @@ func AddAndMultiply(x, y int) (int, int) {
 func swap(age int, name string) (string, int) {
   return name, age
 }
+
+
+func Area(x, y float64) float64 {
+  return x*y
+}
+
+
+func Perimeter(x, y float64) float64 {
+  return 2*(x + y)
+}
+
+
+func ComputeWith34(fn func(float64, float64) float64) float64 {
+  return fn(3, 4)
+}
+
+var hypot = func(a, b float64) float64 {
+              return math.Sqrt(a*a + b*b)
+            }
