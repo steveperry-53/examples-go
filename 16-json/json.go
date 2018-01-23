8eab064d67b46c3a252f68de0d1cd0934599a86f
steveperry-53 <stevepe@google.com>
2018-01-23 03:57:49 +0000

    Add new experiment and pose questions.

--- 16-json/json.go
+++ 16-json/json.go
@@ -37,8 +37,40 @@ func main() {
 	// Trim the leading and trailing quotes.
 	strMarshDesc = strings.TrimPrefix(strMarshDesc, "\"")
 	strMarshDesc = strings.TrimSuffix(strMarshDesc, "\"")
+
+	// To use the description in HTML, we need to replace the unicode excape sequence with an HTML entities.
+	strMarshDesc = strings.Replace(strMarshDesc, "\\u003c", "&lt;", -1)
 	th.Description = strMarshDesc
 
-	// Now th.Description is what we want. It is a string that has an escape sequence for the less-than.
+	// Now th.Description is what we want. It is a string that has an HTML entity for the less-than.
 	fmt.Println(th.Description)
+
+
+	// Next, suppose we have a JSON file that has this data.
+	stData2 := "{\"Name\":\"Endpoints\", \"Description\":\"Endpoints is a collection. Example:\\n  Name: \\\"mysvc\\\"\"}"
+	data2 := []byte(stData2)
+
+	// Unmarshal the JSON data to type Thing.
+	var th2 Thing
+	json.Unmarshal(data2, &th2)
+
+	// Now th2.Description has  regular quote characters and a regular newline character.
+	// That is, the quotes are not escaped. The newline character is not escaped.
+
+	// We could use th2.Description in an HTML document. We wouldn't get new lines at the newline
+	// characters, but other than that, the output would be OK. We wouldn't see weird characters
+	// in the rendered HTML. We wouldn't see \" or \n in the rendered HTML.
+
+	// But we're going to call json.Marshal on the description because there might be angle brackets
+	// in the description. And we need to convert the angle brackets back to Unicode escape sequences
+	// and then to HTML entities.
+
+	// This messes up our quotes and newlines. Now we're going to get \n and \" again in the rendered HTML.
+
+	// We could use strings.Replace to replace \\n with \n and \\\" with \".
+	// But it seems like we're goint in circles.
+
+	// Is there some other way to deal with the angle brackets in descriptios?
+	// Could I skip the json.Marshal step, and instead just replace < with &lt; and replace > with &gt;?
 }
+
