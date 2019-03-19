4666d2f5685b4c9d218479a2f73c2d7162c29fe2
steveperry-53 <stevepe@google.com>
2019-03-18 18:59:12 -0700

    New example for creating and access token.

---
+++ 18-token/token.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"context"
+	"io/ioutil"
+)
+
+func main() {
+
+	var jsonKey []byte
+	jsonKey, _ = ioutil.ReadFile("/home/steve/Documents/keys/edward-02-key.json")
+
+	var ctx context.Context
+	ctx = context.Background()
+
+	var creds *google.Credentials
+	creds, _ = google.CredentialsFromJSON(ctx, jsonKey, "https://www.googleapis.com/auth/bigquery")
+
+	var tknSrc oauth2.TokenSource
+	tknSrc = creds.TokenSource
+	fmt.Println(tknSrc)
+	
+	var tkn *oauth2.Token
+	tkn, _ = tknSrc.Token()
+
+	fmt.Println("\n\ntkn:", (*tkn).AccessToken)
+}
